67cdae44b5fbe9406009170a57233442be04df46
yya290 <yya290@sfu.ca>
2024-04-16 22:06:34 -0700

    Secondary routing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import News from "./News";
+import Message from "./Message";
+import MyNavLink from "../../components/MyNavLink";
+import { Redirect, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 export default class Home extends Component {
   render() {
     return (
-      <h3>I'm Home</h3>
-    )
+      <div>
+        <h3>I'm Home</h3>
+        <div>
+          <ul className="nav nav-tabs">
+            <li>
+              <MyNavLink to="/home/news">News</MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to="/home/message">Message</MyNavLink>
+            </li>
+          </ul>
+          {/* register route */}
+          <Switch>
+            <Route path="/home/news" component={News} />
+            <Route path="/home/message" component={Message} />
+            <Redirect to="/home/news" />
+          </Switch>
+        </div>
+      </div>
+    );
   }
 }
